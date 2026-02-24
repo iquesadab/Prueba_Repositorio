334bdc6e0c61a86545c2bfeb44f02a54d2806c6d
IVANNIA VANESSA QUESADA BOGANTES <iquesadab@ucenfotec.ac.cr>
2026-02-24 16:35:22 -0600

    [updated]: Console.log para gravedad

---
+++ Formularios/js/imc.js
@@ -0,0 +1,26 @@
+
+/* Así se hacen los 
+comentarios multilinea*/
+
+// Comentario de una linea
+
+// console.log es equivalente a system.out.println en Java: Mensaje en consola
+console.log ("Mensaje en consola");
+console.log ("Mensaje en consola");
+
+// Constante: Valor que no cambia durante la ejecución de un programa. El nombre va en mayuscula siempre.
+
+const PI = 3.14;
+console.log(PI);
+
+const MAX_USUARIOS = 100; // Uso guion bajo para separar varias palabras en una constante
+console.log(MAX_USUARIOS);
+
+const GRAVEDAD = 9.18;
+console.log(GRAVEDAD);
+
+const TIPO_USUARIO = "Visitante";
+console.log(TIPO_USUARIO);
+
+// Variable: Valor que cambia durante la ejecución de un programa.
+
